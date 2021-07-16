92ae3493f7d54dca40175c722c3c1f772a2e1722
Lubos-source <73986411+Lubos-source@users.noreply.github.com>
2021-07-16 16:14:12 +0200

    Finall version + docker-compose + Dockerfile
    Mozna jeste dodelat INIT pro postgres, aby mohl uzivatel ihned odzkouset funkcnost na nejake testovaci, nami implementovane, databazi.
    Ale neni nutno ?!

---
+++ APIgen/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE studenti;
+
+CREATE TABLE student
+(
+    id bigint NOT NULL,
+    name text COLLATE pg_catalog."default",
+    CONSTRAINT student_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO student(id, name) VALUES
+ (1, 'A'),
+ (2, 'B'),
+ (3, 'C');
---
+++ APIgen/runCompose.bat
@@ -0,0 +1,3 @@
+set COMPOSE_CONVERT_WINDOWS_PATHS=1
+docker-compose -p APIgen up -d --build
+pause
